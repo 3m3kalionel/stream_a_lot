4459a7f0a9f557dce8a3e1bc1a9aa28a74097ce8
3m3ka <emeka.l.okoro@gmail.com>
2021-02-12 02:35:30 +0100

    - create utility function to validate ids - create utility function to validate documents - create function to fetch artiste - create endpoint for fetching artiste

--- server/controllers/artiste.js
+++ server/controllers/artiste.js
@@ -1,5 +1,5 @@
 import artisteModel from "../models/artisteModel";
-import { handleError } from "../utils";
+import { handleError, validateDocument } from "../utils";
 
 const createArtisteProfile = async (req, res) => {
   try {
@@ -28,6 +28,38 @@ const createArtisteProfile = async (req, res) => {
   }
 };
 
+const getArtiste = async (req, res) => {
+  try {
+    const returnFields = "-createdAt -updatedAt -__v";
+    const fieldsToPopulate = "tracks";
+    const artiste = await validateDocument("artiste", req.params.artisteId, {
+      returnFields,
+      fieldsToPopulate,
+    });
+
+    if (artiste.error) {
+      const {
+        error: { code, message },
+      } = artiste;
+
+      return res.status(code).send({
+        message,
+      });
+    }
+
+    return res.status(200).send({
+      message: "status: success - artiste found",
+      artiste,
+    });
+  } catch (err) {
+    console.log("resssss", err);
+    return res.status(400).send({
+      message: "status: failed - something went wrong",
+    });
+  }
+};
+
 export default {
   createArtisteProfile,
+  getArtiste,
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -7,6 +7,7 @@ const router = app => {
   app.post("/api/v1/user/signup", userController.signup);
   app.post("/api/v1/user/signin", userController.signin);
   app.post("/api/v1/artiste", artisteController.createArtisteProfile);
+  app.get("/api/v1/artiste/:artisteId", artisteController.getArtiste);
 };
 
 export default router;
