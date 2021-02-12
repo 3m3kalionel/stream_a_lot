a76f651f54957fd35fc72ba913765d2767642412
3m3ka <emeka.l.okoro@gmail.com>
2021-02-12 02:35:30 +0100

    - create utility function to validate ids - create utility function to validate documents - create function to fetch artiste - create endpoint for fetching artiste

--- server/utils.js
+++ server/utils.js
@@ -1,3 +1,7 @@
+import mongoose from "mongoose";
+
+import artisteModel from "./models/artisteModel";
+
 export const isEmpty = fieldValue => {
   return !(fieldValue.trim().length === 0);
 };
@@ -10,3 +14,72 @@ export const handleError = ({ errors }, res) => {
     message,
   });
 };
+
+export const isValidMongoId = id => {
+  const ObjectId = mongoose.Types.ObjectId;
+
+  if (ObjectId.isValid(id)) {
+    if (String(new ObjectId(id)) === id) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+};
+
+const getModel = modelParam => {
+  switch (modelParam) {
+    case "playlist":
+      return playlistModel;
+      break;
+    case "track":
+      return gridFsModel;
+      break;
+    case "artiste":
+      return artisteModel;
+      break;
+    case "user":
+      return userModel;
+      break;
+  }
+};
+
+export async function validateDocument(modelParam, documentId, queryOptions) {
+  const error = new Error();
+  const { returnFields, fieldsToPopulate } = queryOptions;
+
+  let document;
+  const model = getModel(modelParam);
+
+  try {
+    if (!isValidMongoId(documentId)) {
+      error.code = 400;
+      error.message = `status: failed - id ${documentId} is not a valid mongoose document id`;
+      throw error;
+    }
+
+    document = returnFields
+      ? await model.findById(documentId, returnFields).populate({
+          path: fieldsToPopulate,
+          select: returnFields,
+        })
+      : await model.findById(documentId, returnFields);
+
+    if (!document) {
+      error.code = 404;
+      error.message = `status: failed - ${modelParam} id ${documentId} not found`;
+      throw error;
+    }
+
+    return document;
+  } catch (error) {
+    if (error.message.includes("is not a valid mongoose document id")) {
+      error.code = 400;
+      return { error };
+    }
+
+    return { error };
+  }
+}
