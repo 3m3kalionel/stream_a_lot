ad10ae7d9c038bb718dc281863e8080602e71710
3m3ka <emeka.l.okoro@gmail.com>
2021-02-12 12:05:09 +0100

    - create function to get request range - check for edge cases in range - create function to get file mimetype - check track validity - return requested track content - create endpoint to fetch track content

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,6 +10,7 @@ const router = app => {
   app.post("/api/v1/artiste", artisteController.createArtisteProfile);
   app.get("/api/v1/artiste/:artisteId", artisteController.getArtiste);
   app.post("/api/v1/track", trackController.createTrack);
+  app.get("/api/v1/track/:trackId", trackController.getTrack);
 };
 
 export default router;
