55bb8551785a4fb74b84e6adb15a0e8984307b3c
3m3ka <emeka.l.okoro@gmail.com>
2021-02-12 03:07:31 +0100

    - add function to create tracks - add endpoint for creating tracks

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,9 @@
-import user from "./user";
 import artiste from "./artiste";
+import track from "./track";
+import user from "./user";
 
 export default {
   artiste,
+  track,
   user,
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,15 @@
 import controllers from "../controllers";
 
-const userController = controllers.user;
 const artisteController = controllers.artiste;
+const trackController = controllers.track;
+const userController = controllers.user;
 
 const router = app => {
   app.post("/api/v1/user/signup", userController.signup);
   app.post("/api/v1/user/signin", userController.signin);
   app.post("/api/v1/artiste", artisteController.createArtisteProfile);
   app.get("/api/v1/artiste/:artisteId", artisteController.getArtiste);
+  app.post("/api/v1/track", trackController.createTrack);
 };
 
 export default router;
